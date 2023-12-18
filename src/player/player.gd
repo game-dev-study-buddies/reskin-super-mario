8bad4090ae4f5b56688d5b1ef0f02542c3ca565e
Christopher Long <93620761+cvalong@users.noreply.github.com>
2023-12-18 11:03:44 -0800

    fix: player sprite animation and floating player on jump

--- src/player/player.gd
+++ src/player/player.gd
@@ -1,14 +1,23 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const DIRECTION_X = {
+	"right": 1,
+	"left": -1,
+}
+
+@export var SPEED := 300.0
+@export var JUMP_VELOCITY := -400.0
+
+@onready var _animation_player = $AnimationPlayer
+@onready var _sprite_2d = $Sprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
+	_animation_player.play("idle")
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -22,15 +31,12 @@ func _physics_process(delta):
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
+		if direction == DIRECTION_X.right:
+			_sprite_2d.flip_h = true
+		else:
+			_sprite_2d.flip_h = false
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * SPEED
-		$AnimatedSprite2D.play()
-	else:
-		$AnimatedSprite2D.stop
 
 	move_and_slide()
 
